17013c2bd62f3c34607c56906a726a96b864eaa7
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-10-04 22:11:21 +0300

    step 1

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 function PetItem({ pet }) {
+  const [image, setImg] = useState(pet.image);
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
         <img className="image" alt={pet.name} src={pet.image} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={() => {
+              setImg(pet.image2);
+            }}
+          >
             Pet
           </button>
           <button type="button" class="btn btn-info  m-2">
